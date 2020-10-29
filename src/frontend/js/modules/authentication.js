6482e7d988121e138a967b7de952a032115abe09
Michael BAILLY <mbailly@linagora.com>
2020-10-29 15:51:00 +0100

    OpenPaaS-Suite/esn-frontend-calendar#241 Cache authentication token
    For direct connections between a frontend & the DAV server, there is a
    need to cache authentication token, otherwise any DAV request will be
    preprocessed with a call to get the authentication token.

--- src/frontend/js/modules/authentication.js
+++ src/frontend/js/modules/authentication.js
@@ -1,17 +1,37 @@
-(function(angular) {
-  'use strict';
+angular.module('esn.authentication', ['esn.http'])
+  .factory('tokenAPI', function(esnRestangular) {
 
-  angular.module('esn.authentication', ['esn.http'])
-    .factory('tokenAPI', function(esnRestangular) {
+    // https://github.com/linagora/openpaas-esn/blob/master/backend/core/auth/token.js#L3
+    // token TTL is 60 seconds server-side. We keep a lower value to get over
+    // network lags
+    const CACHE_MAX_TTL = 45 * 1000;
 
-      function getNewToken() {
-        return esnRestangular.one('authenticationtoken').get();
-      }
+    let cachedToken = { promise: null, timestamp: 0 };
 
-      return {
-        getNewToken: getNewToken
-      };
-    });
-})(angular);
+    return {
+      getNewToken
+    };
+
+    function getCachedToken() {
+      const { promise, timestamp } = cachedToken;
+
+      return timestamp + CACHE_MAX_TTL > Date.now() ? promise : getTokenNoCache();
+    }
+
+    function getTokenNoCache() {
+      const promise = esnRestangular.one('authenticationtoken').get();
+      const timestamp = Date.now();
+
+      promise.then(() => {
+        cachedToken = { promise, timestamp };
+      });
+
+      return promise;
+    }
+
+    function getNewToken(resetCache = false) {
+      return resetCache ? getTokenNoCache() : getCachedToken();
+    }
+  });
 
 require('./http.js');
